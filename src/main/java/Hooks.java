07aa927bed8d0d5b547f41c48504d65341a2621c
Sudip Patel <>
2019-10-02 12:58:46 +0100

    add hooks class

--- src/main/java/Hooks.java
+++ src/main/java/Hooks.java
@@ -4,18 +4,18 @@ import cucumber.api.java.Before;
 
 public class Hooks {
 
-//    private TestBase testBase = new TestBase();
-//
-//    @Before
-//    public void setUp(){
-//        testBase.openBrowser();
-//        testBase.getUrl("http://ec2-34-250-139-60.eu-west-1.compute.amazonaws.com/");
-//        testBase.maximizeBrowser();
-//        testBase.impWaits();
-//    }
-//
-//    @After
-//    public void tearDown(){
-//        testBase.closeBrowser();
-//    }
+    private TestBase testBase = new TestBase();
+
+    @Before
+    public void setUp(){
+        testBase.openBrowser();
+        testBase.getUrl("http://ec2-34-250-139-60.eu-west-1.compute.amazonaws.com/");
+        testBase.maximizeBrowser();
+        testBase.impWaits();
+    }
+
+    @After
+    public void tearDown(){
+        testBase.closeBrowser();
+    }
 }
